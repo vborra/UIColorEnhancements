6bf56b96d4d9709400de626efcb8302f4781e252
Virindh Borra <borra.virindh@gmail.com>
2015-01-20 00:26:53 -0500

    Added Initial Files
    Added initial files, to start building up the methods

---
+++ BetterColor.h
@@ -0,0 +1,12 @@
+//
+//  BetterColor.h
+//  
+//
+//  Created by Virindh Borra on 1/20/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BetterColor : UIColor
+@end
---
+++ BetterColor.m
@@ -0,0 +1,13 @@
+//
+//  BetterColor.m
+//  
+//
+//  Created by Virindh Borra on 1/20/15.
+//
+//
+
+#import "BetterColor.h"
+
+@implementation BetterColor
+
+@end
